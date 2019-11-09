dda578aef1ee29b82421442f7b50bf1508df75a5
Vladislav Starikov <vlad.starikov.88@gmail.com>
2019-11-10 01:12:05 +0300

    git init to get posts

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import Home from './views/Home.vue';
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
@@ -14,10 +15,12 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      component: () => import('./views/About.vue'),
+    },
+    {
+      path: '/posts',
+      name: 'posts',
+      component: () => import('./views/Posts.vue'),
     },
   ],
 });
---
+++ src/views/Posts.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <template v-for="post in posts">
+      <div :key="post.id">
+        <h3>{{ post.author }}</h3>
+        <p>{{ post.message }}</p>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+import axios from '@/plugins/axios';
+
+export default {
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  mounted() {
+    axios
+      .get('/posts')
+      .then((res) => {
+        this.posts = res.data;
+      });
+  },
+};
+</script>
+
+<style>
+
+</style>
